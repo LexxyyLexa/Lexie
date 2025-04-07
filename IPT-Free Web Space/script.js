b89662f177950161067c7ec7c9085e64f0ea4ac0
Alexa Ballenas <alexa.ballenas@hcdc.edu.ph>
2025-04-07 21:36:37 +0600

    Add files via upload

---
+++ IPT-Free Web Space/script.js
@@ -0,0 +1,4 @@
+function changeText() {
+    document.getElementById('title').innerText = "Enjoy exploring!";
+    document.getElementById('description').innerText = "We glad to know that you want to explore! Be our guest!";
+}
